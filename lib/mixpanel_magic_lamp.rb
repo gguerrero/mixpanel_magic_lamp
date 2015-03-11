562f667c0d0d8351e19a501da5642e74840afdad
Guillermo Guerrero <g.guerrero.bus@gmail.com>
2015-03-11 23:45:02 +0100

    Explicit load of submodule files

--- lib/mixpanel_magic_lamp.rb
+++ lib/mixpanel_magic_lamp.rb
@@ -1,5 +1,6 @@
 require 'mixpanel_magic_lamp/engine' if defined? Rails
-require 'mixpanel_magic_lamp/mixpanel'
+require 'mixpanel_magic_lamp/mixpanel/expression_builder'
+require 'mixpanel_magic_lamp/mixpanel/interface'
 
 # Include salt and spieces on the magic lamp
 Mixpanel.include MixpanelMagicLamp::Mixpanel
